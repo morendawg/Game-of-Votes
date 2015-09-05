4e8a0b8c35e4394ac67fa00e0e6e739e323dc64a
Luke Carlson <jlukecarlson@gmail.com>
2015-09-04 22:35:43 -0400

    basic inject code + readme

--- locations.js
+++ locations.js
@@ -1,1 +1,5 @@
 // LOCATIONS
+$("body").contents().each(function () {
+    if (this.nodeType === 3) this.nodeValue = $.trim($(this).text()).replace(/Trump/g, "Joffrey")
+    if (this.nodeType === 1) $(this).html( $(this).html().replace(/Trump/g, "Joffrey") )
+})
