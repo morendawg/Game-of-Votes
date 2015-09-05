dec10e9e3996a39c2682f4ac2517d3978b4a02d2
connorswords <connor.swords@gmail.com>
2015-09-05 16:15:49 -0400

    added .png and .jpg to characterToImages

--- data/characterToImages.js
+++ data/characterToImages.js
@@ -1,164 +1,164 @@
 var characterToImage = {
 	"Tyrion Lannister" :
 		[
-		"http://imgur.com/CHNx8kd", //wide
-		"http://imgur.com/TzWecQy", //square
-		"http://imgur.com/5nep31z"  //tall
+		"http://imgur.com/CHNx8kd.jpg", //wide
+		"http://imgur.com/TzWecQy.jpg", //square
+		"http://imgur.com/5nep31z.jpg"  //tall
 		],
 	"Cersei Lannister":
 		[
-		"http://imgur.com/JfyFzLK", //wide
-		"http://imgur.com/gyo8eKp", //square
-		"http://imgur.com/cqWhEDE"  //tall
+		"http://imgur.com/JfyFzLK.jpg", //wide
+		"http://imgur.com/gyo8eKp.jpg", //square
+		"http://imgur.com/cqWhEDE.png"  //tall
 		],
 	"Daenerys Targaryen":
 		[
-		"http://imgur.com/cQGPw7j", //wide
-		"http://imgur.com/5lK8wUX", //square
-		"http://imgur.com/u0JEzzf"  //tall
+		"http://imgur.com/cQGPw7j.jpg", //wide
+		"http://imgur.com/5lK8wUX.jpg", //square
+		"http://imgur.com/u0JEzzf.jpg"  //tall
 		],
 	"Jon Snow" :
 		[
-		"http://imgur.com/8LK4kXn", //wide
-		"http://imgur.com/T5hwWW0", //square
-		"http://imgur.com/Ab9Q8Kn"  //tall
+		"http://imgur.com/8LK4kXn.jpg", //wide
+		"http://imgur.com/T5hwWW0.jpg", //square
+		"http://imgur.com/Ab9Q8Kn.jpg"  //tall
 		],
 	"Sansa Stark" :
 		[
-		"http://imgur.com/75TUK3a", //wide
-		"http://imgur.com/x3wXZ4I", //square
-		"http://imgur.com/s6koGBc"  //tall
+		"http://imgur.com/75TUK3a.jpg", //wide
+		"http://imgur.com/x3wXZ4I.jpg", //square
+		"http://imgur.com/s6koGBc.jpg"  //tall
 		],
 	"Stannis Baratheon" :
 		[
-		"http://imgur.com/Y4La2gz", //wide
-		"http://imgur.com/TozhR7j", //square
-		"http://imgur.com/nKoKGhc"  //tall
+		"http://imgur.com/Y4La2gz.png", //wide
+		"http://imgur.com/TozhR7j.png", //square
+		"http://imgur.com/nKoKGhc.jpg"  //tall
 		],
 	"Petyr Baelish" :
 		[
-		"http://imgur.com/jDku9TI", //wide
-		"http://imgur.com/wIJ66sE", //square
-		"http://imgur.com/mfcM0GF"  //tall
+		"http://imgur.com/jDku9TI.jpg", //wide
+		"http://imgur.com/wIJ66sE.jpeg", //square
+		"http://imgur.com/mfcM0GF.jpg"  //tall
 		],
 	"The High Sparrow" :
 		[
-		"http://imgur.com/JBWggCV", //wide
-		"http://imgur.com/vgIsj6W", //square
-		"http://imgur.com/8aHSWdB"  //tall
+		"http://imgur.com/JBWggCV.jpg", //wide
+		"http://imgur.com/vgIsj6W.jpg", //square
+		"http://imgur.com/8aHSWdB.png"  //tall
 		],
 	"Robert Baratheon" :
 		[
-		"http://imgur.com/CpRZFiI", //wide
-		"http://imgur.com/4yib79E", //square
-		"http://imgur.com/rbW7NCl"  //tall
+		"http://imgur.com/CpRZFiI.jpg", //wide
+		"http://imgur.com/4yib79E.jpg", //square
+		"http://imgur.com/rbW7NCl.jpg"  //tall
 		],
 	"Ramsay Bolton" :
 		[
-		"http://imgur.com/Ol1X8dM", //wide
-		"http://imgur.com/X4LcSwW", //square
-		"http://imgur.com/PvlMQzG"  //tall
+		"http://imgur.com/Ol1X8dM.png", //wide
+		"http://imgur.com/X4LcSwW.jpg", //square
+		"http://imgur.com/PvlMQzG.png"  //tall
 		],
 	"Eddard Stark" :
 		[
-		"http://imgur.com/tT2Kww5", //wide
-		"http://imgur.com/IOally3", //square
-		"http://imgur.com/rUcFxA1"  //tall
+		"http://imgur.com/tT2Kww5.jpg", //wide
+		"http://imgur.com/IOally3.jpeg", //square
+		"http://imgur.com/rUcFxA1.jpg"  //tall
 		],
 	"Ygritte the Wildling" :
 		[
-		"http://imgur.com/puPDcoc", //wide
-		"http://imgur.com/OPdQD7Y", //square
-		"http://imgur.com/YbdAudH"  //tall
+		"http://imgur.com/puPDcoc.jpg", //wide
+		"http://imgur.com/OPdQD7Y.jpg", //square
+		"http://imgur.com/YbdAudH.jpg"  //tall
 		],
 	"Jorah Mormont" :
 		[
-		"http://imgur.com/aWwfaNc", //wide
-		"http://imgur.com/HscKDNv", //square
-		"http://imgur.com/y85RxCL"  //tall
+		"http://imgur.com/aWwfaNc.png", //wide
+		"http://imgur.com/HscKDNv.jpeg", //square
+		"http://imgur.com/y85RxCL.jpg"  //tall
 		],
 	"Hodor" :
 		[
-		"http://imgur.com/A3G55eT", //wide
-		"http://imgur.com/kH7O4qA", //square
-		"http://imgur.com/hQeUygf"  //tall
+		"http://imgur.com/A3G55eT.png", //wide
+		"http://imgur.com/kH7O4qA.jpg", //square
+		"http://imgur.com/hQeUygf.jpg"  //tall
 		],
 	"Lord Varys" :
 		[
-		"http://imgur.com/o4wXkoA", //wide
-		"http://imgur.com/3Eqejde", //square
-		"http://imgur.com/AfNndiV"  //tall
+		"http://imgur.com/o4wXkoA.jpg", //wide
+		"http://imgur.com/3Eqejde.jpeg", //square
+		"http://imgur.com/AfNndiV.png"  //tall
 		],
 	"Joffrey Baratheon" :
 		[
-		"http://imgur.com/jAOs92B", //wide
-		"http://imgur.com/PFbITQ7", //square
-		"http://imgur.com/EfE2heK"  //tall
+		"http://imgur.com/jAOs92B.jpeg", //wide
+		"http://imgur.com/PFbITQ7.jpeg", //square
+		"http://imgur.com/EfE2heK.png"  //tall
 		],
 	"Bronn" :
 		[
-		"http://imgur.com/u7EjpJq", //wide
-		"http://imgur.com/nwLUbvQ", //square
-		"http://imgur.com/Y9lH4Zp"  //tall
+		"http://imgur.com/u7EjpJq.jpg", //wide
+		"http://imgur.com/nwLUbvQ.jpg", //square
+		"http://imgur.com/Y9lH4Zp.jpg"  //tall
 		],
 	"Oberyn Martell" :
 		[
-		"http://imgur.com/DJhfp8f", //wide
-		"http://imgur.com/8cAlHlK", //square
-		"http://imgur.com/xiKlXj8"  //tall
+		"http://imgur.com/DJhfp8f.jpg", //wide
+		"http://imgur.com/8cAlHlK.jpg", //square
+		"http://imgur.com/xiKlXj8.jpg"  //tall
 		],
 	"Podrick Payne" :
 		[
-		"http://imgur.com/a5F1rlz", //wide
-		"http://imgur.com/6VJIL7E", //square
-		"http://imgur.com/7OWJBZN"  //tall
+		"http://imgur.com/a5F1rlz.jpg", //wide
+		"http://imgur.com/6VJIL7E.jpg", //square
+		"http://imgur.com/7OWJBZN.png"  //tall
 		],
 	"Samwell Tarly" :
 		[
-		"http://imgur.com/J42N8bs", //wide
-		"http://imgur.com/rZ2KsPf", //square
-		"http://imgur.com/SXhUxto"  //tall
+		"http://imgur.com/J42N8bs.jpg", //wide
+		"http://imgur.com/rZ2KsPf.jpg", //square
+		"http://imgur.com/SXhUxto.jpg"  //tall
 		],
 	"Maester Aemon" :
 		[
-		"http://imgur.com/CeFVPXo", //wide
-		"http://imgur.com/B9fTs7S", //square
-		"http://imgur.com/Ssp8aKi"  //tall
+		"http://imgur.com/CeFVPXo.jpg", //wide
+		"http://imgur.com/B9fTs7S.jpg", //square
+		"http://imgur.com/Ssp8aKi.jpg"  //tall
 		],
 	"Sandor Clegane" :
 		[
-		"http://imgur.com/tE6IVhT", //wide
-		"http://imgur.com/VoFmOo7", //square
-		"http://imgur.com/YyJr5Zt"  //tall
+		"http://imgur.com/tE6IVhT.jpg", //wide
+		"http://imgur.com/VoFmOo7.png", //square
+		"http://imgur.com/YyJr5Zt.jpg"  //tall
 		],
 	"Grey Worm" :
 		[
-		"http://imgur.com/O2nLxUU", //wide
-		"http://imgur.com/Hb8R5Nv", //square
-		"http://imgur.com/W8ZzIYB"  //tall
+		"http://imgur.com/O2nLxUU.png", //wide
+		"http://imgur.com/Hb8R5Nv.jpg", //square
+		"http://imgur.com/W8ZzIYB.jpg"  //tall
 		],
 	"Jojen Reed" :
 		[
-		"http://imgur.com/HCApMjY", //wide
-		"http://imgur.com/Qf1Omoi", //square
-		"http://imgur.com/CEhWJIW"  //tall
+		"http://imgur.com/HCApMjY.jpg", //wide
+		"http://imgur.com/Qf1Omoi.jpg", //square
+		"http://imgur.com/CEhWJIW.jpg"  //tall
 		],
 	"Theon Greyjoy" :
 		[
-		"http://imgur.com/WxmhgNs", //wide
-		"http://imgur.com/G1qFPzR", //square
-		"http://imgur.com/mOwy1Xb"  //tall
+		"http://imgur.com/WxmhgNs.jpg", //wide
+		"http://imgur.com/G1qFPzR.jpg", //square
+		"http://imgur.com/mOwy1Xb.jpg"  //tall
 		],
 	"Ellaria Sand" :
 		[
-		"http://imgur.com/NXBspNU", //wide
-		"http://imgur.com/qCHpSm6", //square
-		"http://imgur.com/xuEEXGZ"  //tall
+		"http://imgur.com/NXBspNU.jpg", //wide
+		"http://imgur.com/qCHpSm6.jpg", //square
+		"http://imgur.com/xuEEXGZ.jpg"  //tall
 		],
 	"Ser Davos" :
 		[
-		"http://imgur.com/ocarhvJ", //wide
-		"http://imgur.com/hThq3Q3", //square
-		"http://imgur.com/td6Wddh"  //tall
+		"http://imgur.com/ocarhvJ.jpg", //wide
+		"http://imgur.com/hThq3Q3.jpg", //square
+		"http://imgur.com/td6Wddh.jpg"  //tall
 		]
 };
