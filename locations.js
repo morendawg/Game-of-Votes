5d2312f5f4548d304dbf31d26d1df8a6137aeb5c
Luke Carlson <jlukecarlson@gmail.com>
2015-09-04 23:36:37 -0400

    more rules + keywords

--- locations.js
+++ locations.js
@@ -1,17 +1,43 @@
 // LOCATIONS
 
 var rules = {
+    "Donald J. Trump": "Joffrey",
     "Donald Trump": "Joffrey",
     "Trump": "Joffrey",
 
+    "Hillary Rodham Clinton": "Catelyn Stark",
     "Hillary Clinton": "Catelyn Stark",
     "Hillary": "Catelyn Stark",
     "Clinton": "Catelyn Stark"
 }
 
-var html = $("body").html();
-for (key in rules) {
-    html = html.replace(key,rules[key])
-}
-$("body").html(html);
+$("body").contents().each(function () {
+    if (this.nodeType === 3) {
+        var text = $.trim($(this).text());
+        
+        for (key in rules) {
+            console.log(key +"-> " + rules[key]);
+            text = text.replace(key,rules[key]);
+        }
+        this.nodeValue = text;
+    }
+    if (this.nodeType === 1) {
+        var text = $(this).html();
+        for (key in rules) {
+            text = text.replace(key,rules[key]);
+        }
+        $(this).html( text )
+    }
+    // To reduce calls just do
 
+
+})
+
+    /*
+      var html = $("body").html();
+      for (key in rules) {
+      html = html.replace(key,rules[key])
+      }
+      $("body").html(html);
+
+    */
