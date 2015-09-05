2dc172c91cc91e3bf5e22a340953af2f173aa0ee
Luke Carlson <jlukecarlson@gmail.com>
2015-09-04 23:25:39 -0400

    rm unused code

--- locations.js
+++ locations.js
@@ -9,29 +9,6 @@ var rules = {
     "Clinton": "Catelyn Stark"
 }
 
-/*
-  $("body").contents().each(function () {
-  if (this.nodeType === 3) {
-  var text = $.trim($(this).text());
-  
-  for (key in rules) {
-  text = text.replace(key,rules[key]);
-  }
-  this.nodeValue = $.trim($(this).text()).replace(/Trump/g, "Joffrey")
-  }
-  if (this.nodeType === 1) {
-  var text = $(this).html();
-  for (key in rules) {
-  text = text.replace(key,rules[key]);
-  }
-  $(this).html( text )
-  }
-  // To reduce calls just do
-
-
-  })*/
-
-
 var html = $("body").html();
 for (key in rules) {
     html = html.replace(key,rules[key])
