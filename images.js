6352ea454610df7d86514deb73e11b1ca1889704
Luke Carlson <jlukecarlson@gmail.com>
2015-09-05 13:11:07 -0400

    started image work, moved loc to char

--- images.js
+++ images.js
@@ -1,1 +1,34 @@
 // IMAGES
+var images = {
+    misc: 'http://i.imgur.com/aXd8vwt.jpg',
+    clinton: '',
+    trump: '',
+    carson: '',
+    bush: '',
+    cruz: '',
+    rubio: '',
+    walker: '',
+    fiorina: '',
+    huckabee: '',
+    kasich: '',
+    christie: '',
+    paul: '',
+    perry: '',
+    santorum: '',
+    jindal: '',
+    pataki: ''
+};
+
+$('img').each(function() {
+    console.log($(this).attr('src'));
+    /*
+    for (character in images) {
+        if (s.indexOf(character) > -1) {
+            $(this).attr('src',images[character]);
+            return;
+        }
+    }
+    // if nothing found then just change it
+    */
+    $(this).attr('src',images.misc);
+});
