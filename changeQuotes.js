e6d0a2b98fabfd441e9ee4e9d90c82367fb3bf2a
Luke Carlson <jlukecarlson@gmail.com>
2015-09-06 01:29:53 -0400

    re added quotes to the gamer

--- changeQuotes.js
+++ changeQuotes.js
@@ -1,10 +1,8 @@
-var changeQuotes = function() {
-    $('p').each(function() {
-        var text = $(this).text();
-        var match = text.match(/“([^]*)”/);
-        if (match) {
-            text = text.replace(/“([^]*)”/g,'"' + randomFrom(quotes) + '"');
-            $(this).text(text);
-        }
-    });
-}
+$('p').each(function() {
+    var text = $(this).text();
+    var match = text.match(/“([^]*)”/);
+    if (match) {
+        text = text.replace(/“([^]*)”/g,'"' + randomFrom(quotes) + '"');
+        $(this).text(text);
+    }
+});
